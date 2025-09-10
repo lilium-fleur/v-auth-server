52d5dec747bca75e18a8b0eff1050b4dda8bb8f2
haruxime <haruxime01@gmail.com>
2025-09-10 17:02:59 +0400

    Init

---
+++ src/test/java/org/fleur/vauthserver/OauthResourceServerApplicationTests.java
@@ -0,0 +1,13 @@
+package org.fleur.oauthresourceserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OauthResourceServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
